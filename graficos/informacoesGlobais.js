60e074bc3e938ec6921a48c481b14ba42b59bf88
JL show <128520433+JoaoLucas2B@users.noreply.github.com>
2024-11-27 13:34:50 -0300

    27/11jkjkbjjh

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,7 +6,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `O time mais caro do mundo é o Real Madrid com um custo de <span>${dados.Real_Madrid} bilhões de reais</span>, em segundo lugar temos o Manchester City, no valor de <span>${dados.Manchester_City} bilhões</span>, na terceira posição é o Arsenal, valendo <span>${dados.Arsenal}</span> bilhões de reais. Os dois últimos mais caros, respectivamente, são o: Chelsea e Barcelona, com <span>${dados.Chelsea} bilhões </span> e <span>${dados.Barcelona} bilhões de reais</span>.`
+    paragrafo.innerHTML = `O time mais caro do mundo é o Real Madrid com um custo de <span>${dados.Real_Madrid} bilhões de reais</span>, em segundo lugar temos o Manchester City, no valor de <span>${dados.Manchester_City} bilhões</span>, na terceira posição é o Arsenal, valendo <span>${dados.Arsenal} bilhões de reais</span>. Os dois últimos mais caros, respectivamente, são o: Chelsea e Barcelona, com <span>${dados.Chelsea} bilhões </span> e <span>${dados.Barcelona} bilhões de reais</span>.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
